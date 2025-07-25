2f0d10a49b876269e29b65fe9b84a648956b5b9a
sandyknight <sandyknight90@gmail.com>
2025-07-25 17:38:43 +0100

    add 2025 UTLA codes

--- R/get-utla-codes.R
+++ R/get-utla-codes.R
@@ -12,8 +12,18 @@ get_utla_codes <-
         "LAD24CD"
       )
 
+    lkp25 <-
+      sgapi::get_table_link_lookup(
+        lookup_table = "LAD25_CTYUA25_EW_LU_v2",
+        "CTYUA25NM",
+        "LAD25NM",
+        "CTYUA25CD",
+        "LAD25CD"
+      )
 
     data.table::setDT(lkp24)
+    data.table::setDT(lkp25)
+
     pull_utla_codes <- function(table_link) {
       data.table::setDT(table_link)
 
@@ -24,9 +34,21 @@ get_utla_codes <-
       table_link
     }
 
-    utla_codes <- pull_utla_codes(lkp24)
+    utla_codes24 <- pull_utla_codes(lkp24)
+    utla_codes25 <- pull_utla_codes(lkp25)
+
+    utla_codes <-
+      data.table::rbindlist(l = list(
+        utla_codes24,
+        utla_codes25
+      ))
 
     data.table::setnames(utla_codes, c("utlanm", "utlacd"))
 
-    utla_codes
+    utla_codes <- unique(utla_codes)
   }
+
+
+utla_codes <- get_utla_codes()
+
+utla_codes
